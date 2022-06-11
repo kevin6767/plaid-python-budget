3b5e5d7d187e0b515d556162573029d35f9b64d9
eslickkevin <kevin@chekhub.com>
2022-06-11 00:24:06 -0400

    removed keys

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-from datetime import datetime
 import plaid
+import os
+import pygsheets
+from datetime import datetime
 from plaid.api import plaid_api
 from plaid.model.accounts_balance_get_request import AccountsBalanceGetRequest
 from plaid.model.transactions_get_request import TransactionsGetRequest
-import pygsheets
-import numpy as np
-
-# Available environments are
-# 'Production'
-# 'Development'
-# 'Sandbox'
 from plaid.model.transactions_get_request_options import TransactionsGetRequestOptions
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+SECRET = os.getenv('SECRET')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+SERVICE_FILE = os.getenv('SERVICE_FILE')
+EMAIL = os.getenv('EMAIL')
 
 configuration = plaid.Configuration(
     host=plaid.Environment.Development,
     api_key={
-        'clientId': "6261ad2ca538fb001a150a6a",
-        'secret': "ebaadda6199f7aa1348d24ce920439",
+        'clientId': CLIENT_ID,
+        'secret': SECRET,
     }
 )
 
@@ -24,7 +28,7 @@ api_client = plaid.ApiClient(configuration)
 client = plaid_api.PlaidApi(api_client)
 
 request = TransactionsGetRequest(
-    access_token="access-development-e9bedab4-46af-48d4-8117-d556447927a1",
+    access_token=ACCESS_TOKEN,
     start_date=datetime.strptime('2022-05-1', '%Y-%m-%d').date(),
     end_date=datetime.strptime('2022-06-9', '%Y-%m-%d').date(),
     options=TransactionsGetRequestOptions(
@@ -33,11 +37,11 @@ request = TransactionsGetRequest(
 )
 response = client.transactions_get(request)
 transactions = response['transactions']
-request_account = AccountsBalanceGetRequest(access_token="access-development-e9bedab4-46af-48d4-8117-d556447927a1")
+request_account = AccountsBalanceGetRequest(access_token=ACCESS_TOKEN)
 response_account = client.accounts_balance_get(request_account)
 accounts = response['accounts']
 
-gc = pygsheets.authorize(service_file='service_account_credentials.json')
+gc = pygsheets.authorize(service_file=SERVICE_FILE)
 
 print('opening sheets')
 # Open spreadsheet and then worksheet
@@ -80,42 +84,4 @@ balance.set_value(checking_balance)
 net_gain.set_value(sum(deposits_cleaned) - sum(expenses))
 
 print('cleaning up')
-sh.share("kevin.eslick4156@gmail.com")
-
-# {'account_id': 'RXN3yXrL6EH7O0ZabgvaTKNo1Ad4MmU5JwR3n',
-#  'account_owner': None,
-#  'amount': 74.83,
-#  'authorized_date': None,
-#  'authorized_datetime': None,
-#  'category': ['Shops', 'Supermarkets and Groceries'],
-#  'category_id': '19047000',
-#  'check_number': None,
-#  'date': datetime.date(2022, 5, 2),
-#  'datetime': None,
-#  'iso_currency_code': 'USD',
-#  'location': {'address': None,
-#               'city': 'Newark',
-#               'country': None,
-#               'lat': None,
-#               'lon': None,
-#               'postal_code': None,
-#               'region': 'DE',
-#               'store_number': None},
-#  'merchant_name': 'Acme Markets',
-#  'name': 'CHECKCARD 0429 ACME 2680 NEWARK DE XXXXX5221XXXXXXXXXX0298',
-#  'payment_channel': 'in store',
-#  'payment_meta': {'by_order_of': None,
-#                   'payee': None,
-#                   'payer': None,
-#                   'payment_method': None,
-#                   'payment_processor': None,
-#                   'ppd_id': None,
-#                   'reason': None,
-#                   'reference_number': None},
-#  'pending': False,
-#  'pending_transaction_id': None,
-#  'personal_finance_category': None,
-#  'transaction_code': None,
-#  'transaction_id': 'vbzZabKNmeH8R49YOXvYi4ZgqPDvweHJDQzoL',
-#  'transaction_type': 'place',
-#  'unofficial_currency_code': None}
+sh.share(EMAIL)
